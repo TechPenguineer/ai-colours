ea8856cddd0d3f19e8176fb0608df3adc6ca09bc
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-19 09:27:10 -0400

    create color matrix

--- source/Form1.cs
+++ source/Form1.cs
@@ -17,13 +17,23 @@ namespace source
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
             Image image = new Bitmap("InputColour.bmp");
             ImageAttributes imageAttributes = new ImageAttributes();
 
             int width = image.Width;
             int height = image.Height;
+
+            float[][] colorMatrixElements =
+            {
+                     new float[] {2,  0,  0,  0, 0},       
+                     new float[] {0,  1,  0,  0, 0},        
+                     new float[] {0,  0,  1,  0, 0},        
+                     new float[] {0,  0,  0,  1, 0},       
+                     new float[] {.2f, .2f, .2f, 0, 1}
+        };
+            ColorMatrix colorMatrix = new ColorMatrix(colorMatrixElements);
         }
     }
 }
